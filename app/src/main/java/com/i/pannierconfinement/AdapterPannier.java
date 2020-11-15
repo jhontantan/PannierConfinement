8a86966717eeac0a209c37175bd1f99770b3b850
jhontantan <jomar404@gmail.com>
2020-11-15 09:50:49 +0100

    V1.3

--- app/src/main/java/com/i/pannierconfinement/AdapterPannier.java
+++ app/src/main/java/com/i/pannierconfinement/AdapterPannier.java
@@ -23,59 +23,40 @@ public class AdapterPannier extends RecyclerView.Adapter<AdapterPannier.Produits
     List<Produits> pannier;
     double total=0;
 
-    public AdapterPannier(Context context, TextView tvTotal, List<Produits> pannier) {
+    public AdapterPannier(Context context, List<Produits> pannier, TextView tvTotal) {
         this.context = context;
         this.tvTotal = tvTotal;
         this.pannier = pannier;
 
         for (int i = 0 ; i < pannier.size() ; i++) {
             total = total + Double.parseDouble(""+pannier.get(i).getPrix());
-
         }
+
+        tvTotal.setText(""+total);
     }
 
     @NonNull
     @Override
     public ProduitsViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_rv_produits,null,false);
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_rv_pannier,null,false);
         return new AdapterPannier.ProduitsViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ProduitsViewHolder produitsViewHolder, int position) {
-        produitsViewHolder.tvNombProduit.setText(listProduits.get(position).getNomProd());
-        produitsViewHolder.tvDescProduit.setText(listProduits.get(position).getDesc());
-        produitsViewHolder.tvPrix.setText(""+listProduits.get(position).getPrix());
+        produitsViewHolder.tvNombProduit.setText(pannier.get(position).getNomProd());
+        produitsViewHolder.tvDescProduit.setText(pannier.get(position).getDesc());
+        produitsViewHolder.tvPrix.setText(""+pannier.get(position).getPrix());
 
-        produitsViewHolder.cbPanier.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (produitsViewHolder.cbPanier.isChecked() == true) {
-                    tvCmptProduct.setText(""+Integer.parseInt(tvCmptProduct.getText().toString().trim())+1);
-                    pannier.add(listProduits.get(position));
-                }
-            }
-        });
-
-        btnPanier.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(context, Pannier.class);
-                intent.putExtra("Pannier", (Serializable) pannier );
-                context.startActivity(intent);
-
-            }
-        });
     }
 
     @Override
     public int getItemCount() {
-        return listProduits.size();
+        return pannier.size();
     }
 
     public class ProduitsViewHolder extends RecyclerView.ViewHolder {
         TextView tvNombProduit, tvDescProduit, tvPrix;
-        CheckBox cbPanier;
 
         public ProduitsViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -83,7 +64,6 @@ public class AdapterPannier extends RecyclerView.Adapter<AdapterPannier.Produits
             tvNombProduit = itemView.findViewById(R.id.tvNombProduit);
             tvDescProduit = itemView.findViewById(R.id.tvDescProduit);
             tvPrix = itemView.findViewById(R.id.tvPrix);
-            cbPanier = itemView.findViewById(R.id.cbPanier);
 
 
         }
